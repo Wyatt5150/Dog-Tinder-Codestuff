7f846672683f611f5476f26e5af8c3c714fb4059
wyatt ryan <wyatt.mahoney5150@gmail.com>
2024-11-07 19:19:39 -0600

    ProfileView scene is mostly functional for a user, does not provide dog profile viewing yet

--- Datez4Dogz/Scripts/profile_view.gd
+++ Datez4Dogz/Scripts/profile_view.gd
@@ -1,4 +1,5 @@
 extends Node2D
+@export var profileButton:PackedScene
 var profile: UserProfile
 
 var images:Array
@@ -6,23 +7,34 @@ var imageIndex:int
 
 var buttons
 
-var fireOnce
+var holdingAbout:bool
+var aboutContainer:Control
+var prevY:float
+var aboutVel:float
+var aboutMaxY:float
+var aboutMinY:float
+
+const aboutAcc:float = 700
+const verPadding:float = 10
+const horPadding:float = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fireOnce = true
-	
-	imageIndex = 0
+	holdingAbout = false
+	aboutContainer = $About
+	aboutVel = 0
+	aboutMaxY = 584
+	aboutMinY = 0
 	
 	# set up image display stuff
 	var next = $UserImageDisplay/Next
 	var prev = $UserImageDisplay/Prev
 	var backdrop = $UserImageDisplay
 	
-	# set up image area
-	backdrop.position.x = get_viewport().size.x/2 - backdrop.size.x/2
+	# set up image 
+	backdrop.position.x = get_viewport().get_visible_rect().size.x/2 - backdrop.size.x/2
 	backdrop.get_node("UserImage").size = backdrop.size
-	
+
 	# set buttons to change image
 	prev.position.x = 0
 	prev.position.y = backdrop.size.y/2-prev.size.y/2
@@ -35,10 +47,30 @@ func _ready() -> void:
 	for but in buttons:
 		but.disabled = true
 	
-	load_profile(Globals.undecided[0])
-	pass # Replace with function body.
+	$About.size.x = get_viewport().get_visible_rect().size.x - horPadding*2
+	$About.position.x = verPadding
 	
-
+	load_profile(Globals.undecided[0])
+	pass
+	
+func _process(delta: float) -> void:
+	if holdingAbout:
+		aboutVel = (get_global_mouse_position().y-prevY)*(1/delta)
+		aboutContainer.position.y += aboutVel*delta
+		prevY = get_global_mouse_position().y
+	else:
+		if aboutContainer.position.y > aboutMaxY:
+			aboutVel = 0
+			aboutContainer.position.y = move_toward(aboutContainer.position.y, aboutMaxY, 1000*delta)
+		elif aboutContainer.position.y < aboutMinY:
+			aboutVel = 0
+			aboutContainer.position.y = move_toward(aboutContainer.position.y, aboutMinY, 1000*delta)
+		# glide
+		elif aboutVel != 0:
+			aboutContainer.position.y += aboutVel*delta
+		
+			aboutVel = move_toward(aboutVel, 0, aboutAcc*delta)
+	
 func load_profile(userProfile:UserProfile):
 	profile = userProfile
 	load_info()
@@ -51,17 +83,55 @@ func load_info(isUserProfile:bool = true):
 	set_profile_image(0)
 	
 	# load information
-	var info = $About/VBoxContainer
-	info.get_node("Name").text = profile.username
-	info.get_node("About").text = profile.about
-
+	var info = $About
+	var container = $About/Container 	
+		
+	var username = container.get_node("Name")
+	username.text = profile.username
+	
+	load_profiles()
+	
+	var about = container.get_node("About")
+	about.text = profile.about
+	about.position.y = username.position.y + get_label_height(username) + verPadding
+	
+	# determine about container size
+	var viewportHeight = get_viewport().get_visible_rect().size.y
+	info.size.y = max(about.position.y + get_label_height(about) + verPadding*2 + 100, viewportHeight - verPadding*2)
+	info.position.y = aboutMaxY
+	aboutMinY = -(info.size.y - viewportHeight) - verPadding
+	
+	# set margins
+	container.size = info.size - Vector2(horPadding*2, verPadding*2)
+	container.position = Vector2(horPadding, verPadding)
+	
+	# position text
+	about.position.y = username.position.y + get_label_height(username) + verPadding
+	
 # Profile Image Display things
 func set_profile_image(index):
 	index += profile.pictures.size()
 	index = index%profile.pictures.size()
 	imageIndex = index
-	$UserImageDisplay/UserImage.texture = images[imageIndex]
+	$UserImageDisplay/UserImage.texture = images[index]
+
+func get_label_height(label:Label):
+	return label.get_line_count()*(label.get_line_height()+label.get_theme_constant("line_spacing"))
 
 func change_image(incr: int) -> void:
 	set_profile_image(imageIndex+incr)
-	pass # Replace with function body.
+	pass
+
+func load_profiles():
+	pass
+	
+
+func pressed_about() -> void:
+	prevY = get_global_mouse_position().y
+	holdingAbout = true
+	pass
+
+
+func released_about() -> void:
+	holdingAbout = false
+	pass
